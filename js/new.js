cf9ca796fa6706d27d37213b642860e0b20eebc5
jopraveen <jopraveensupergaming@example.com>
2021-01-10 14:24:45 +0530

    Added page views count

--- js/new.js
+++ js/new.js
@@ -41,3 +41,15 @@
     });    
 })();
 
+// For view counter
+const countEl = document.getElementById('count');
+
+updateVisitorsCount();
+
+function updateVisitorsCount() {
+    fetch('https://api.countapi.xyz/update/jopraveen.github.com/jopraveen/?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value;
+        });
+}
