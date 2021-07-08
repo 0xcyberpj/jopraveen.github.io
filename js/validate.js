882402e22fa6ccc498c0732c68bf86269753b613
jopraveen <jopraveensupergaming@example.com>
2021-07-08 23:10:22 +0530

    ctf

---
+++ js/validate.js
@@ -0,0 +1,25 @@
+// This is not the right place hacker o.O
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+
+function validate(){
+
+    if (password.value === "admin" | username.value==="admin"){
+        alert("Hamcker spotted!!")
+    }
+    else if(password.value.includes("script") | username.value.includes("script")){
+        alert("XSS? 😆😆😆")
+    }
+    else if(password.value.includes("ls") | username.value.includes("ls")){
+        alert("noob.txt\nflag.txt\nnasa hacking.pdf\ncreds.txt\ninsert_brain.html")
+    }
+    else if(password.value.includes("whoami") | username.value.includes("whoami")){
+        alert("I'm jopraveen✋")
+    }
+    else{
+        alert("TEST")
+        window.location = "hamcker.txt"
+    }
+
+}
